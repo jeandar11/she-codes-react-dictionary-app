ccdad9ed41d87a62d270682d96ca022dd096c318
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-06-22 09:53:38 -0400

    Bootstrap installation
    + Code change to allow Netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
+        <button className="btn btn-primary">Hello</button>
       </header>
     </div>
   );
