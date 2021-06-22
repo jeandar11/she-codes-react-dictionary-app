72b5a5cd4e92e1f0408794e205e57dba8238a289
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-06-22 10:43:02 -0400

    Search form implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <button className="btn btn-primary">Hello</button>
-      </header>
+      <div className="container">
+        <header>
+          <h1>Dictionary App</h1>
+          <h2>A useless book full of information</h2>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            {" "}
+            <a
+              href="https://github.com/jeandar11/she-codes-react-dictionary-app"
+              target="_blank"
+            >
+              Open-cource code
+            </a>{" "}
+            by Jeanne Darcel thanks to{" "}
+            <a href="https://shecodes.io" target="_blank">
+              SheCodes
+            </a>{" "}
+            👩‍💻
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for "${keyword}"...`);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autofocus="on"
+          autocomplete="off"
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
